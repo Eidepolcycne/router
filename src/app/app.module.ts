fc44c969315d2e5407c4fd989a4ef32f595a0652
Eidepolcycne <fbab1@free.fr>
2023-01-11 12:25:12 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { MenuComponent } from './menu/menu.component';
+import { ROUTES } from './app.routes';
+import { UserComponent } from './user/user.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    MenuComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
